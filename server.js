0ddfeade42488b809d880b73de66ea0d231aecc7
laukikN <laukiknirgudkar@gmail.com>
2014-01-04 12:30:08 +0530

    little problems

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express'),http = require('http');
 var app = express();
 var server = http.createServer(app).listen(5000);
 var io = require('socket.io').listen(server);
-
+var k = 1;
 /*    middlewares          */
 
 app.set('views', __dirname + '/views');
@@ -16,10 +16,39 @@ app.use(express.static(__dirname + '/'));
 
 
 app.get('/',function(req,res){
-	//res.render('problem.ejs',{statement : "aaaaaaaaa"});
-	res.render('problem.ejs',{ statement:"WHAT IS 2*2 ?" , option: ["1","2","3","4"] });
+	console.log('/');
+	//res.render('problem.ejs',{ statement:"WHAT IS 2*2 ?" , option: ["1","2","3","4"] });
+	redis.lrange('Quiz:0',0,-1,function(err,quiz){
+		if(err) res.send('quiz not found');
+		else{
+			console.log(quiz);
+			for(var i=0; i<quiz.length;i++){
+				!function syc(i){
+					redis.lindex('Question',quiz[i],function(err,quest){
+						if(err) res.send('unable to get questions');
+						else{
+							redis.lrange('Option:'+quiz[i],0,-1,function(err,opt){
+								if(err) res.send('unable to fetch options');
+								else{
+									console.log(quest);
+									console.log(opt);
+
+									
+									res.render('problem.ejs',{ statement : quest, option : opt });	
+									if(k == 1){
+										console.log('asdadadad');
+										k++;
+									}
+								}
+							});
+
+						}
+					});
+				}(i)
+			}
+		}
+	});
 	
-	console.log("/");
 
 });
 
