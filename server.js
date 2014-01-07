ed130c1388fd21eabe9eff97edb1ad17f873d11d
laukikN <laukiknirgudkar@gmail.com>
2014-01-07 18:38:41 +0530

    server

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var app = express();
 var server = http.createServer(app).listen(5000);
 var io = require('socket.io').listen(server);
 var path = require("path");
+var number = [];
+var correct = [];
 var x = 0;
 var state,solv;
 /*    middlewares          */
@@ -36,12 +38,17 @@ app.post('/signin', function (req, res){
 app.post('/validate',function (req,res){
 	var user = req.param('uid');
 	var passwd = req.param('passwd');
-	redis.hget('user',user,function(err,pass){
+	redis.lindex(user,0,function(err,pass){
 		if(err) res.send(404);
 		else{
 			if(pass){
 				if( pass == passwd ){
-					res.redirect('/quesz');
+					number.push(user);
+					number.push(0);
+					correct.push(user);
+					correct.push(0);
+					res.render('select.ejs',{ title : user })
+					//res.redirect(307, '/quesz?user='+user);
 				}else{
 					res.render('login.ejs',{ title : "Wrong Password" });
 				}
@@ -52,32 +59,39 @@ app.post('/validate',function (req,res){
 	});
 });
 
+app.post("/create",function (req,res){
+	
+});
 
-
-
-
-
-app.get('/quesz',function(req,res){
+app.post('/quesz',function(req,res){
+	var taker = req.param('user');
+	console.log("asdasdasd " + taker);
+	var curr;
+	for(var i = 0; i<number.length; i+=2){
+		if(number[i] === taker){
+			x = number[i+1];
+			break;
+		}
+	}
 	console.log('/');
-	console.log(req.param("opti"));
+	if ( req.param("opti") == req.param("ans") ){
+		correct[correct.indexOf(taker)+1]++;
+	}
 	if( x >= 3 ){
-		res.render("end.ejs");
+		res.render("end.ejs",{ score : correct[correct.indexOf(taker)+1]-1 });
 	}else{
+		console.log("aaaaaaa " + x);
 		redis.lindex('Quiz:0',x,function(err,quiz){
 			if(err) res.send('quiz not found');
 			else{
-			//console.log(quiz);
 				redis.lindex('Question',quiz,function(err,quest){
 					if(err) res.send('unable to get questions ' + x);
 					else{
 						redis.lrange('Option:'+quiz,0,-1,function(err,opt){
 							if(err) res.send('unable to fetch options');
 							else{
-								
-								//console.log(quest);
-								//console.log(opt);
-								x++;
-								res.render('problem.ejs',{ statement : quest, option : opt });	
+								number[number.indexOf(taker)+1]++;
+								res.render('problem.ejs',{ statement : quest, option : opt , title : taker});	
 							}
 						});
 					}
@@ -85,21 +99,23 @@ app.get('/quesz',function(req,res){
 			}
 		});
 	}
-	//res.render('problem.ejs',{ statement:"WHAT IS 2*2 ?" , option: ["1","2","3","4"] });
 });
 
 
-/*			  socket.io stuff		 */
+/*			  socket.io stuff		 */ 
 
 
 io.sockets.on('connection',function(socket){
 	
-	socket.on('newuser',function(name,pass){
-        redis.hset('user',name,pass,function(err,status){
-            if(err) throw err;
-        	    socket.emit('forward');
-            });
+	socket.on('newuser',function(name,pass,email){
+        redis.rpush(name,pass,email,0,0,"0:0:0", function (err, status){
+        	if(err) console.log("RPUSH FOR USER DOSEN'T WORK");
+        	redis.hset("email-user",email,name,function (err, result){
+        		if(err) console.log("reverse mapping FOR USER DOSEN'T WORK");
+        		socket.emit('forward');
+        	});
         });
+    });
 
     socket.on('check',function (name){
     	console.log(name);
@@ -125,15 +141,25 @@ io.sockets.on('connection',function(socket){
 			}
     	});
     });
-
-
 	socket.on('disconnect',function(){
 		
 	});
-
 });
 
 
+setInterval(function(){
+	for(var i=0; i<number.length; i+=2){
+		redis.lset(number[i],2,number[i+1],function (err ,result){
+			if(err) console.log("UNABLE TO Dump current Question ID");
+		});
+	}
+	for(var i=0; i<correct.length; i+=2){
+		redis.lset(correct[i],3,correct[i+1],function (err ,result){
+			if(err) console.log("UNABLE TO Dump correct Questions");
+		});
+	}
+	console.log("Dump in progress");
+},(1000*60*3));
 
 
 
